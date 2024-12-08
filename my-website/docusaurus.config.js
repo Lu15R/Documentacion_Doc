959ead88a15b79dbc55ed91624e6687d30ba1328
Luis Carlos <ruberrodriguezcarlos@gmail.com>
2024-12-07 20:00:53 -0600

    [FIX]:corregui un error en el archivo .gitignore

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -15,10 +15,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://Lu15R.github.io',
+  url: 'https://docusaurus-f42ds0lv9-luisrs-projects-5daf8219.vercel.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/Documentacion_Doc',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
